53153b1b06b575d8cb362ddf4a54ca09a9c5c42d
Albeoris <albeoris@gmail.com>
2025-08-25 03:03:21 +0200

    Add debug messages

--- Albeoris.AsiNetLoader/dllmain.cpp
+++ Albeoris.AsiNetLoader/dllmain.cpp
@@ -2,20 +2,36 @@
 #include <thread>
 #include <windows.h>
 
+#define ASI_NET_LOADER_WRITE_DEBUG_MESSAGES
+
+void WriteDebugMessage(const char* text)
+{
+#ifdef ASI_NET_LOADER_WRITE_DEBUG_MESSAGES
+    std::cout << text << std::endl;
+#endif
+}
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
     // std::this_thread::sleep_for(std::chrono::milliseconds(30000));
-    std::cout << "HOOKED" << std::endl;
-    
+
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        WriteDebugMessage("DllMain: DLL_PROCESS_ATTACH");
+        break;
     case DLL_THREAD_ATTACH:
+        WriteDebugMessage("DllMain: DLL_THREAD_ATTACH");
+        break;
     case DLL_THREAD_DETACH:
+        WriteDebugMessage("DllMain: DLL_THREAD_DETACH");
+        break;
     case DLL_PROCESS_DETACH:
+        WriteDebugMessage("DllMain: DLL_PROCESS_DETACH");
+        break;
+    default:
         break;
     }
-	
+
     return TRUE;
 }
-
